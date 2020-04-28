1d597dc0d8ce875f52a1ccf00956b62ce2f05e3e
Lim Kyung Min <limup3>
2020-04-28 15:13:16 +0900

    15:10

--- bit2/src/main/java/com/bit/web/user/UserService.java
+++ bit2/src/main/java/com/bit/web/user/UserService.java
@@ -3,25 +3,13 @@ package com.bit.web.user;
 import java.util.List;
 
 public interface UserService {
-
 	public void add(User user);
-
 	public int count();
-
 	public User login(User user);
-	
 	public User detail(String userid);
-
 	public boolean update(User user);
-
 	public boolean remove(String userid);
-
 	public List<User> list();
-
-	public boolean idSearch(String userid);
+	public boolean idSearch(String user);
 	
-	public void saveFile(User user);
-	
-	public List<User> readFile();
-
 }
--- bit2/src/main/java/com/bit/web/user/UserServiceImpl.java
+++ bit2/src/main/java/com/bit/web/user/UserServiceImpl.java
@@ -21,89 +21,94 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements UserService {
 	
-	private Map<String, Object> map; 
-	private int count ;
-	//Map의 키값은 String , 상수풀에 있어야 빨리찾는다
-	//<> 엔트리 배열
-	public UserServiceImpl() {
-		map = new HashMap<>();
-		count = 0;
-	}
+	public final static String FILE_PATH = "C:\\Users\\bit\\spring-workspace\\occamsrazor\\src\\main\\resources\\static\\user\\";
+
 	@Override
 	public void add(User user) {
-		map.put(user.getUserid(), user);
+		try {
+			File file = new File(FILE_PATH+"list.csv");
+			@SuppressWarnings("resource")
+			BufferedWriter writer = new BufferedWriter(new FileWriter(file, true));
+					String message = user.toString();
+					System.out.println(message);
+					writer.write(message);
+					writer.newLine();
+					writer.flush();
+		} catch(Exception e) {
+			System.out.println("파일 입력 시 에러 발생");
+		}
+		
 	}
+
 	@Override
 	public int count() {
-		
-		return map.size();
+		return 0;
 	}
+
 	@Override
 	public User login(User user) {
 		User returnUser = null;
-		if(map.containsKey(user.getUserid())) {
-			User u = detail(user.getUserid());
-			if(user.getPasswd().equals(u.getPasswd())) {
-				returnUser = u;
-				
-			}
-		}
+		
 		return returnUser;
 	}
+
 	@Override
 	public User detail(String userid) {
-		System.out.println("id"+userid);
-		User t = (User) map.get(userid);
-		System.out.println("map"+t);
-		return (User) map.get(userid);
+		
+		return null;
 	}
+
 	@Override
 	public boolean update(User user) {
-		
-		map.replace(user.getUserid(), user);
 		return true;
 	}
+
 	@Override
 	public boolean remove(String userid) {
-		
-		map.remove(userid);
-		
 		return true;
 	}
-	@SuppressWarnings({ "rawtypes", "unchecked" })
 	@Override
 	public List<User> list() {
-		List<User> list = new ArrayList<>();
-		Set set = map.entrySet();
-		Iterator it = set.iterator();
-		while(it.hasNext()) {
-			Map.Entry<String, User> e = (Entry<String, User>) it.next();
-			list.add(e.getValue());
-			
+		List<User> userlist = new ArrayList<>();
+		List<String> list = new ArrayList<>();
+		try {
+			File file = new File(FILE_PATH+"list.csv");
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			String message = "";
+			while((message = reader.readLine()) != null) {
+				list.add(message); 
+			}
+			reader.close();
+		} catch(Exception e) {
+			System.out.println("파일 읽기에서 에러 발생");
 		}
-		for(int i = 0 ; i<list.size() ; i++) { 
-			System.out.println(list.get(i));
+		User u = null;
+		for(int i =0;i < list.size(); i++) {
+			u = new User();
+			String[] arr = list.get(i).split(",");
+			u.setUserid(arr[0]);
+			u.setPasswd(arr[1]);
+			u.setName(arr[2]);
+			u.setSsn(arr[3]);
+			u.setAddr(arr[4]);
+			userlist.add(u);
 		}
-		return list;
+		return userlist;
 	}
-	
-	
 	
 	@Override
 	public boolean idSearch(String userid) {
-		boolean ok = false;
-		
+		boolean ok = true;
+		List<User> list = list();
+		for(int i=0; i<list.size(); i++) {
+			if(userid.equals(list.get(i).getUserid())) {
+				ok = false;
+				break;
+			}
+		}
 		return ok;
 	}
-	@Override
-	public void saveFile(User user) {
-		// TODO Auto-generated method stub
-		
-	}
-	@Override
-	public List<User> readFile() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+
+
 	
 }
