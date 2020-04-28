584ccae3ccb84bef29508d53b89e9e87c28d5ea2
Lim Kyung Min <limup3>
2020-04-28 12:45:40 +0900

    2020 04 28 12:45

---
+++ bit2/src/main/java/com/bit/web/Bit2Application.java
@@ -0,0 +1,13 @@
+package com.bit.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bit2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bit2Application.class, args);
+	}
+
+}
